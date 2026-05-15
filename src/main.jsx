60896bb3f5cdd47c9124fbc50b599767ff0be9a5
kvasmisso-sketch <kvas.misso@gmail.com>
2026-05-15 16:11:09 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(reg => {
+    console.log('Service Worker зарегистрирован');
+  }).catch(err => {
+    console.log('Помилка Service Worker:', err);
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
